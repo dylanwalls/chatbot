d51aa23dae978395f2852d4de100f23192c8dbcc
dylanwalls <dylan.walls@bitprop.com>
2024-04-26 11:34:31 +0200

    on

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
-    console.log(`Server running port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
 
 app.post('/webhook', async (req, res) => {
